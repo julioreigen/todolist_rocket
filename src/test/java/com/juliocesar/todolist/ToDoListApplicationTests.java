82602349b64255aa32dcb835d92c48f0023ea661
Julio <juliobrito.tec@gmail.com>
2023-10-14 12:30:54 -0300

    Dockerfile deploy

---
+++ src/test/java/com/juliocesar/todolist/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliocesar.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
